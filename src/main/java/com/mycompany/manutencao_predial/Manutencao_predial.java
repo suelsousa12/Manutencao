71fb32964f583cd9d90d88865a3a4ef9c7826abc
suelsousa12 <suel.sousa12@gmail.com>
2025-02-03 10:53:02 -0300

    mandando projeto inicial

---
+++ src/main/java/com/mycompany/manutencao_predial/Manutencao_predial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manutencao_predial;
+
+/**
+ *
+ * @author devmat
+ */
+public class Manutencao_predial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
